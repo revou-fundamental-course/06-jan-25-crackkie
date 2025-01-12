0d2c4616f35249e77227aaa5b5e26c5a53a23cd3
crackkie <craz493@gmail.com>
2025-01-13 00:37:59 +0700

    add  responsif and ham menu

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let hamMenu = document.getElementById('hamMenu');
+let navMenu = document.getElementById('navMenu');
+
+function menu(){
+    navMenu.classList.toggle('active');
+    hamMenu.classList.toggle('active');
+
+}
+
+
