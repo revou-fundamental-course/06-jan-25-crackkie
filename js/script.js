23d1d9126b3b4af919c22e0088db0468fb052dd7
crackkie <craz493@gmail.com>
2025-01-16 14:36:04 +0700

    revamp

--- js/script.js
+++ js/script.js
@@ -6,6 +6,50 @@ function menu(){
     hamMenu.classList.toggle('active');
 
 }
+var slideIndex = 1;
+function showDivs(n){
+    var i;
+    var imgList = document.getElementsByClassName('slide');
+    if (n > imgList.length){slideIndex = 1};
+    if (n < 1){slideIndex = imgList.length};
+    for(i = 0; i < imgList.length; i ++){
+        imgList[i].style.display = 'none';
+    }
+    imgList[slideIndex -1].style.display = 'block';
+}
+function plusDivs(n){
+    showDivs(showDivs(slideIndex += n))
+}
+showDivs(slideIndex)
+setInterval(() =>{
+    plusDivs(1);
+},3000)
+
+window.onload = greeting()
+
+function greeting(){
+    document.getElementById('greting-input').style.display = 'flex';
+
+    document.getElementById('button-greting').addEventListener('keydown', () =>{
+        if (Event.key === "Enter") {
+            console.log("Tombol Enter ditekan!");
+            gretingForm = document.getElementById('gretingName').value;
+            gretingName = document.getElementById('greeting-user');
+            gretingName.innerHTML = gretingForm;
+            document.getElementById('greting-input').style.display = 'none'
+        }
+    })
+
+
+    document.getElementById('button-greting').addEventListener('click', () =>{
+        gretingForm = document.getElementById('gretingName').value;
+        gretingName = document.getElementById('greeting-user');
+        gretingName.innerHTML = gretingForm;
+        document.getElementById('greting-input').style.display = 'none'
+    })
+
+
+}
 
 function validation(){
     let name = document.getElementById('fname').value.trim();
