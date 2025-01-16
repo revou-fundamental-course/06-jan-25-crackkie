086603c0a5b92f0ca2db68f75cbafbf92f694c27
crackkie <craz493@gmail.com>
2025-01-16 20:50:08 +0700

    Finsh

--- js/script.js
+++ js/script.js
@@ -1,99 +1,100 @@
-let hamMenu = document.getElementById('hamMenu');
-let navMenu = document.getElementById('navMenu');
-let submitButton = document.getElementById('submit')
-function menu(){
-    navMenu.classList.toggle('active');
-    hamMenu.classList.toggle('active');
+let slides = document.getElementsByClassName('image');
+indexSlide = 0;
 
-}
-var slideIndex = 1;
-function showDivs(n){
-    var i;
-    var imgList = document.getElementsByClassName('slide');
-    if (n > imgList.length){slideIndex = 1};
-    if (n < 1){slideIndex = imgList.length};
-    for(i = 0; i < imgList.length; i ++){
-        imgList[i].style.display = 'none';
+function changeIndex(n){
+    indexSlide += n
+   
+    if (indexSlide >= slides.length) {
+        indexSlide = 0; 
+    } else if (indexSlide < 0) {
+        indexSlide = slides.length - 1;  
     }
-    imgList[slideIndex -1].style.display = 'block';
+   
+    changeSlide()
 }
-function plusDivs(n){
-    showDivs(showDivs(slideIndex += n))
+
+function changeSlide(){
+
+    for (let i =0; i < slides.length; i++){
+        slides[i].classList.remove('active');
+    }
+
+    slides[indexSlide].classList.add('active');
+
 }
-showDivs(slideIndex)
-setInterval(() =>{
-    plusDivs(1);
-},3000)
+        
 
-window.onload = greeting()
+// changeSlide()
+setInterval(changeIndex,3000,1);
 
-function greeting(){
-    document.getElementById('greting-input').style.display = 'flex';
+window.onload = greetingPopup();
 
-    document.getElementById('button-greting').addEventListener('keydown', () =>{
-        if (Event.key === "Enter") {
-            console.log("Tombol Enter ditekan!");
-            gretingForm = document.getElementById('gretingName').value;
-            gretingName = document.getElementById('greeting-user');
-            gretingName.innerHTML = gretingForm;
-            document.getElementById('greting-input').style.display = 'none'
+function greetingPopup(){
+    let button = document.getElementById('submit-greeting');
+    let popup = document.getElementById('popup');
+    popup.style.display = 'flex'
+    document.body.style.overflow = 'hidden'
+
+    button.addEventListener('click', ()=>{
+        let nameUser = document.getElementById('greeting-form').value;
+
+        if(nameUser.trim() === ""){
+            document.getElementById('eror').innerHTML = 'Please Enter Your Name ';
+            document.getElementById('greeting-form').style.border = '2px solid red'
+            return false;
+    
+        }else{
+            document.body.style.overflow = ''
+            document.getElementById('eror').innerHTML = '';
+            popup.style.display = 'none'
+            document.getElementById('user-name').innerHTML = nameUser;
         }
+
     })
+};
 
-
-    document.getElementById('button-greting').addEventListener('click', () =>{
-        gretingForm = document.getElementById('gretingName').value;
-        gretingName = document.getElementById('greeting-user');
-        gretingName.innerHTML = gretingForm;
-        document.getElementById('greting-input').style.display = 'none'
-    })
-
-
-}
+document.getElementById('submit').addEventListener('click', ()=>{
+    console.log('click')
+    validation()
+})
 
 function validation(){
-    let name = document.getElementById('fname').value.trim();
+    let notify = document.getElementById('alert')
+    let doneNotify = document.getElementById('doneNotify');
+    let name = document.getElementById('fname').value;
     let dob = document.getElementById('fbirth').value;
     let genderMale = document.getElementById('genderMale');
     let genderFemale = document.getElementById('genderFemale');
     let message = document.getElementById('message').value;
-    let notify = document.getElementById('alert');
-    let doneNotify = document.getElementById('doneNotify');
 
-
-    if (name === ''){
+    if (name ===''){
         notify.innerHTML = 'Name cannot be empty!';
-        return
+        return;
     }
-
-    if (dob === '') {
+    if(dob ===''){
         notify.innerHTML = 'Date Of Birth cannot be empty!';
-        return
-    } 
-
-    if (genderMale.checked === false && genderFemale.checked === false) {
-        notify.innerHTML = 'Gender cannot be empty!';
-        return
+        return;
     }
-    
-    if (message === '') {
-        notify.innerHTML = 'At least write some word!';
-        return
-    }else{
-        notify.innerHTML = ''
+   if(genderMale.checked === false && genderFemale.checked === false){
+        notify.innerHTML = 'Gender cannot be empty!';
+        return ;
+   }
+   if(message ===''){
+        notify.innerHTML = 'At leas write one word!';
+        return;
+   }else{
+        notify.innerHTML ='';
         doneNotify.innerHTML = 'Sent successfully'
-        const now = new Date();
-        document.getElementById('time').innerHTML =  now;
-
+        document.getElementById('time').innerHTML = Date();
         document.getElementById('name-message').innerHTML = name;
         document.getElementById('birth-message').innerHTML = dob;
-        if(genderMale.checked === true){
-            document.getElementById('gender-message').innerHTML ='Male';
-        }else{
-            document.getElementById('gender-message').innerHTML ='Female';
-        }
-        document.getElementById('message-container').innerHTML = message
-    }
 
+        if(genderMale.checked === true){
+            document.getElementById('gender-message').innerHTML = 'Male';
+        }else{
+            document.getElementById('gender-message').innerHTML = 'Female';
+        }
+
+        document.getElementById('message-container').innerHTML = message;
+   }
 }
-submitButton.addEventListener("click", validation);
